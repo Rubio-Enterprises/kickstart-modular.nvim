2302f9f086150c8b6a8478114c28fe34e432f8cc
Steven <105327421+strubio-ray@users.noreply.github.com>
2026-09-07 14:13:49 -0500

    feat(editor): enable treesitter folds and foldcolumn

--- lua/options.lua
+++ lua/options.lua
@@ -21,6 +21,11 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
 
+-- Enable fold column and keep folds open by default
+vim.o.foldcolumn = '1'
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
